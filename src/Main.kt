40fffd720783f3bd4e7962a2b11040ea65ed51b9
Hossam Abd Elkader <hossam203055@gmail.com>
2024-04-04 15:33:14 +0200

    test

---
+++ src/Main.kt
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val user = "Hossam203055"
+    println("Hello, " + user + "!")
+    println("Count i with me!")
+    for (i in 1..5) {
+        println("i = $i")
+    }
+    println("Hello! This is new edit")
+}
